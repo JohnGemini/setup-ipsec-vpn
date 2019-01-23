b14156a7bf279f561940b3151d9f1598f3a9a9e6
JohnGemini <john.chung@geminiopencloud.com>
2019-01-23 07:53:35 +0000

    use config file to set up vpn connection

--- vpn_connect.sh
+++ vpn_connect.sh
@@ -2,8 +2,12 @@
 GATEWAY=$(ip route |grep default|grep "via \S*" -o|cut -f2 -d" ")
 VPN_SERVER_IP=$(grep lns /etc/xl2tpd/xl2tpd.conf |grep "\S*$" -o)
 ACTION=$1
-VPN_NET=$2
+CIDR_CONFIG=$(dirname $0)/cidr.txt
 if [ "$ACTION" == "up" ]; then
+  if [ ! -f "$CIDR_CONFIG" ]; then
+    echo Cannot find cidr.txt
+    exit 1
+  fi
   service strongswan restart
   service xl2tpd restart
   ipsec up vpn
@@ -13,7 +17,10 @@ if [ "$ACTION" == "up" ]; then
   do
     sleep 1
   done
-  ip route add $VPN_NET dev ppp0
+  for VPN_NET in $(cat $CIDR_CONFIG)
+  do
+    [[ $VPN_NET =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))?$ ]] && ip route add $VPN_NET dev ppp0
+  done
 elif [ "$ACTION" == "down" ]; then
   echo "d vpn" > /var/run/xl2tpd/l2tp-control
   ipsec down vpn
